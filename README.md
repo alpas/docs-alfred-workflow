dfd1e28959719ed7dd5aa9fd64f60f689ca25354
Ashok Gelal <ashokgelal@gmail.com>
2020-02-11 21:56:08 -0700

    First release

---
+++ README.md
@@ -0,0 +1,21 @@
+# [Alpas Docs](https://alpas.dev/docs) Workflow for Alfred
+
+Alpas docs search workflow for Alfred.
+
+![Screenshot](screenshot.png)
+
+## Installation
+
+1. [Download the latest version](https://github.com/alpas/docs-alfred-workflow/releases/download/v1.0.0/AlpasDocs.alfredworkflow)
+2. Double-click the `.alfredworkflow` file to install it.
+3. Add the workflow to a category, then click `Import`.
+
+## Usage
+
+Type `al` followed by a search term to see the result. While selected, hit `⌘+y` for quickly viewing the documentation of the selected search result or hit enter to browse it online.
+
+```
+al <query>
+```
+
+![Search by Algolia](algolia.png)
---
+++ alpas.php
@@ -0,0 +1,63 @@
+<?php
+
+use Alfred\Workflows\Workflow;
+use AlgoliaSearch\Client as Algolia;
+use AlgoliaSearch\Version as AlgoliaUserAgent;
+
+require __DIR__.'/vendor/autoload.php';
+
+$query = $argv[1];
+
+$workflow = new Workflow;
+$parsedown = new Parsedown;
+$algolia = new Algolia('BH4D9OD16A', 'e017bb0b99fe3193f750d48a4e7d441e');
+
+AlgoliaUserAgent::addSuffixUserAgentSegment('Alpas Docs Alfred Workflow', '1.0.0');
+
+$index = $algolia->initIndex('alpas');
+$search = $index->search($query);
+$results = $search['hits'];
+
+$subtextSupported = $subtext === '0' || $subtext === '2';
+
+if (empty($results)) {
+    if (empty($results)) {
+        $workflow->result()
+                 ->title('No matches')
+                 ->icon('google.png')
+                 ->subtitle("No match found in the docs. Search Google for: \"Alpas+{$query}\"")
+                 ->arg("https://www.google.com/search?q=alpas+{$query}")
+                 ->quicklookurl("https://www.google.com/search?q=alpas+{$query}")
+                 ->valid(true);
+
+        echo $workflow->output();
+        exit;
+    }
+    exit;
+}
+
+$urls = [];
+
+
+foreach ($results as $hit) {
+    $highestLvl = $hit['hierarchy']['lvl6'] ? 6 : ($hit['hierarchy']['lvl5'] ? 5 : ($hit['hierarchy']['lvl4'] ? 4 : ($hit['hierarchy']['lvl3'] ? 3 : ($hit['hierarchy']['lvl2'] ? 2 : ($hit['hierarchy']['lvl1'] ? 1 : 0)))));
+
+    $title = $hit['hierarchy']['lvl'.$highestLvl];
+    $currentLvl = 0;
+    $subtitle = $hit['hierarchy']['lvl0'];
+    while ($currentLvl < $highestLvl) {
+        $currentLvl = $currentLvl + 1;
+        $subtitle = $subtitle.' » '.$hit['hierarchy']['lvl'.$currentLvl];
+    }
+
+    $workflow->result()
+             ->uid($hit['objectID'])
+             ->title($title)
+             ->autocomplete($title)
+             ->subtitle($subtitle)
+             ->arg($hit['url'])
+             ->quicklookurl($hit['url'])
+             ->valid(true);
+}
+
+echo $workflow->output();
